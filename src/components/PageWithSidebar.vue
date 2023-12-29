9f4ae96c0b7280d2562b3f34dd14d23bb7e89b12
Tyler <tfritzy@gmail.com>
2023-12-29 00:02:51 -0800

    Hiding side bar for now

--- src/components/PageWithSidebar.vue
+++ src/components/PageWithSidebar.vue
@@ -3,13 +3,13 @@ import MyBio from './MyBio.vue'
 </script>
 
 <template>
-  <div class="h-screen w-screen flex justify-center bg-neutral-900">
-    <div class="flex flex-row items-center w-[1024px]">
-      <MyBio class="flex-[1]" />
+  <div class="h-screen w-screen flex justify-center">
+    <div class="flex flex-row items-center">
+      <!-- <MyBio id="sidebar" class="flex-[1]" /> -->
 
-      <div class="h-screen w-[1px] m-8 bg-neutral-700" />
+      <!-- <div class="h-screen w-[1px] m-8 bg-neutral-700" /> -->
 
-      <div class="flex-[3] max-h-screen overflow-y-auto py-[150px] pr-[15px]">
+      <div class="flex-[3] max-h-screen overflow-y-auto max-w-[800px] py-[150px] p-4">
         <slot name="content" />
       </div>
     </div>
